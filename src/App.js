d2dbdc9ec1aafa21491ebe52f07daefe6c70ed2d
Arda Turkvan <43078044+ArdaTurkvan@users.noreply.github.com>
2024-01-08 15:23:37 -0500

    Reverts branch back to early stages of site for second attempt

--- src/App.js
+++ src/App.js
@@ -1,85 +1,87 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-
-// Styles
 import './style/App.css';
-import './style/aboutme.css';
-import './style/project.css';
-import './style/social.css';
-import './style/navbar.css';
-import './style/text.css';
-
-// Fonts
 import './fonts/coolvetica.otf'
-
-// Components
-import Projects from './components/Projects.js';
-import anime from 'animejs';
-import Collapsible from './components/Collapsible';
-import WavyText from './components/WavyText';
-import AboutMe from './components/AboutMe';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
-import Hover from './components/Hover';
+import Project from './components/Project'
 
 // note: make the page beach themed cause i love beaches :D
-
-//TODO: make functions that return different sections to pass to the Collapsibles!
-
 export default function App() {
 
-  const animationRef = React.useRef(null);
+  const projects = [
+    // Software Engineering projects
+    {
+      type: "software",
+      name: "Picshare",
+      year: "2021",
+      desc: "An online image editing platform with real-time collaboration. Users can hop in and out of drawing rooms, save images to their profile, and reload them for continued editing in the future.",
+      skills: ["React", "MongoDB", "JavaScript", "PeerJS", "AnimeJS", "Bootstrap", "CSS"]
+    },
 
-  React.useEffect(() => {
-    animationRef.current = anime.timeline();
-    animationRef.current.add({
-      targets: '.cool-header .letter',
-      translateY: [-500, 0],
-      opacity: [0.0, 1.0],
-      duration: 800,
-      easing: 'easeOutSine',
-      direction: 'normal',
-      delay: anime.stagger(60)
-    })
-    .add({
-      targets: '.subheader',
-      translateY: [50, 0],
-      opacity: [0.0, 1.0],
-      duration: 1000,
-      easing: 'easeOutSine'
-    });
-  });
+    {
+      type: "software",
+      name: "Path Tracing Renderer",
+      year: "2022",
+      desc: "A rendering algorithm that uses computer graphics math techniques to generate realistic visuals! Features include: Texture / Alpha / Normal Mapping, Area light sources, Reflection & Refraction, Depth of Field, Importance sampling, Accelerated rendering through use of Bounded Volue Hierarchies (BVH)",
+      skills: ["C", "Linear Algebra", "Statistics"]
+    },
 
+    {
+      type: "software",
+      name: "BugByte",
+      year: "2023",
+      desc: "A top-down 2D pixel-art dungeon-crawler roguelite creature-catching game. Build an ever-changing team of quirky creatures and keep them alive through strategic combat as you traverse deeper into the Drive to uncover the mysteries of the plague.",
+      skills: ["Unity", "C#", "Aseprite", "Photoshop"]
+    },
 
-  function nameTitle() {
-    return (
-      <Hover>
-        <div className='wavy-text header cool-header'>
-          <h1>
-            <span>{[..."Arda"].map((letter, index) => <span key={index} className='letter'>{letter}</span>)}</span>
-            <span>{[..."Turkvan"].map((letter, index) => <span key={index} className='letter'>{letter}</span>)}</span>
-          </h1>
-        </div>
-      </Hover>
-    )
-  }
+    // Game Design projects
+    {
+      type: "game",
+      name: "Break the Card Tower",
+      year: "2023",
+      desc: "desc",
+      skills: ["a skill"]
+    },
+
+    {
+      type: "game",
+      name: "Attack on Titan: Division",
+      year: "2023",
+      desc: "desc",
+      skills: ["a skill"]
+    }
+  ]
+
 
   return (
     <div className="App">
-      <Navbar />
-      <div id="home" className='parallax header-container'>
-        {nameTitle()}
-        <div className='subheader-wrap'>
-          <h2 className='subheader cool-subheader'><WavyText text="Software Engineer | "></WavyText></h2>
-          <h2 className='subheader cool-subheader'><WavyText text="Game Designer"></WavyText></h2>
-        </div>
-      </div>
+      <h1>Arda Turkvan</h1>
+      <h2>Software Engineer, Game Designer, Nerd</h2>
+      <p><a href="https://ca.linkedin.com/in/arda-turkvan-8a6a96211" target="_blank">LinkedIn</a></p>
+      <p><a href="https://github.com/ArdaTurkvan" target="_blank">GitHub</a></p>
+      <p><a href="https://www.instagram.com/arda_farda/" target="_blank">Instagram</a></p>
 
-      <div className='scrolling-background page-container'>
-        <AboutMe />
-        <Projects />
-        <Contact />
-      </div>
+      <h2>About Me</h2>
+      <p>
+        I'm a graduate of the University of Toronto Scarborough Computer Science program, specializing
+        in Software Engineering.
+      </p>
+      <p>I'm currently enrolled in the Postgraduate Game Design program at George Brown College.</p>
+      <p>One of my proudest accomplishments is writing a path tracing renderer from scratch in C.</p>
+      <p>
+        My favorite pastimes include playing MapleStory and Hearthstone, <a href="https://www.instagram.com/arda_farda/" target="_blank">drawing & painting</a>,
+        and biking along the Beaches in the summer!
+      </p>
+
+      <h2>Software Engineering (include images)</h2>
+      {
+        projects.filter((project) => project.type === "software").map((project) => 
+        <Project name={project.name} year={project.year} desc={project.desc} skills={project.skills} />)
+      }
+
+      <h2>Game Design (include images)</h2>
+      {
+        projects.filter((project) => project.type === "game").map((project) => 
+        <Project name={project.name} year={project.year} desc={project.desc} skills={project.skills} />)
+      }
+
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,72 +1,17 @@
-import React from 'react';
-import { useInView } from "react-intersection-observer";
-import '../style/project.css';
-import '../style/App.css';
-import HeaderBubble from './HeaderBubble';
-import anime from 'animejs';
-import BodyBubble from './BodyBubble';
-import Hover from './Hover';
-import Skills from './Skills';
+import React from 'react'
 
 /*
  * A component dedicated to displaying a project that I've worked on
  */
-
-// TODO: Add a hard skills, soft skills distinction
-export default function Project({ name, url, year, content, skills }) {
-
-  const animationRef = React.useRef(null);
-  const selfRef = React.useRef(null);
-
-
-  const [viewRef, inView] = useInView({
-    triggerOnce: true,
-    rootMargin: '0px 0px',
-    delay: 800
-  })
-
-  React.useEffect(() => {
-    animationRef.current = anime({
-      targets: selfRef.current,
-      loop: false,
-      duration: 1000,
-      scale: [0.8, 1.0],
-      opacity: [0.0, 1.0],
-      direction: 'normal',
-      easing: 'easeOutQuad'
-    });
-
-    if (inView === true) {
-      animationRef.current.reset();
-      animationRef.current.play();
-    } else {
-      animationRef.current.reset();
-      animationRef.current.pause();
-    }
-  }, [inView]);
-
-  // Body bubble hover handlers
-
+export default function Project({ name, year, desc, skills }) {
   return (
-    <div className='project content-container'>
-      <div ref={selfRef}>
-        <Hover>
-          <HeaderBubble title={name} url={url}>
-            <span className="project-year">{year}</span>
-          </HeaderBubble>
-        </Hover>
-
-        <BodyBubble child={
-          <div ref={viewRef}>
-            <div className='description'>{content}</div>
-            <div className='skills-section'>
-              <h5 className="skills-header short-shadow">Skills</h5>
-              <Skills skillsList={skills}/>
-            </div>
-          </div>
-        }></BodyBubble>
-
-      </div>
+    <div>
+        <h3>{name} {year}</h3>
+          <p>{desc}</p>
+          <h4>Skills</h4>
+          <ul>
+            {skills.map((skill) => <li>{skill}</li>)}
+          </ul>
     </div>
   )
 }
